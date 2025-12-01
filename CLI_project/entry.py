a2b78cf8ae3dd3933c58143eeff941f7844417fd
flavius841 <flaviusepintilie@gmail.com>
2025-12-01 08:54:19 +0200

    update

--- CLI_project/entry.py
+++ CLI_project/entry.py
@@ -1,2 +1,21 @@
+import requests
+
+
+# def handle_request(website, output_file):
+#     r = requests.get(website)
+#     print('Received response')
+#     if output_file is not None:
+#         with open(output_file, 'w') as f:
+#             f.write(r.content.decode())
+#         print(f'Wrote to {output_file}')
+
+
+
+# def handle_print():
+#     print ("You can do this")
+
+
 def cli_entry_point():
     print ("Hello World")
+
+
